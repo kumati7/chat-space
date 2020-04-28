ca37cd1ceaeb80bf82351c5f393c2854effd6350
kumati7 <kumati1208@gmail.com>
2020-04-28 14:42:30 +0900

    デプロイ4

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, 'ruby 2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/wheyprotein_pem.pem'] 
+                  keys: ['~/.ssh/wheyprotein.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
